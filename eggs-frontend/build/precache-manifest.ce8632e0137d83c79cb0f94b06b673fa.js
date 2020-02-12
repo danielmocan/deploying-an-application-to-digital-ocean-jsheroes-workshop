9902191245d2521ba095a97b4b5d5b33b619ab10
Daniel Mocan <contact_daniel@yahoo.com>
2020-02-12 18:18:17 +0200

    Added build

---
+++ eggs-frontend/build/precache-manifest.ce8632e0137d83c79cb0f94b06b673fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfce329ab9e3a8dbb27c3da55683a49f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3f1376b69396c0a1a275",
+    "url": "/static/css/main.c0025d6a.chunk.css"
+  },
+  {
+    "revision": "3c8e891575f40c49ef79",
+    "url": "/static/js/2.322198b7.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.322198b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f1376b69396c0a1a275",
+    "url": "/static/js/main.211555f4.chunk.js"
+  },
+  {
+    "revision": "dc4ed26b040306620998",
+    "url": "/static/js/runtime-main.1fe4ae53.js"
+  }
+]);
